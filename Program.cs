c1fe26e7f46e46da98b1cf9331b736ebf867e897
Shubhamrazz <sr99845@gmail.com>
2022-05-15 15:10:22 +0530

    [Shubham]Adding Element To BST

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Welcome Message
+            Console.WriteLine("-----Welcome To Binary Search Tree Program-----");
+
+            //Creating binary search tree to insert and display node values(UC1)
+            BinarySearchTree_LNR<int> binaryTree = new BinarySearchTree_LNR<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Insert(22);
+            binaryTree.Insert(40);
+            binaryTree.Insert(60);
+            binaryTree.Insert(95);
+            binaryTree.Insert(11);
+            binaryTree.Insert(65);
+            binaryTree.Insert(3);
+            binaryTree.Insert(16);
+            binaryTree.Insert(63);
+            binaryTree.Insert(67);
+
+            //Display inputs
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Created A Binary Search Tree");
+            Console.WriteLine("--------------------------------");
+
+            binaryTree.Display();
+        }
+    }
+}
